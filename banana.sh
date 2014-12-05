5cd810b66f40b29be9effc9829030a822dc7145b
proto <alessandro@protodigital.net>
2014-12-05 10:26:26 +0100

    get a banana

---
+++ banana.sh
@@ -0,0 +1,35 @@
+get_banana() { 
+
+cat <<"EOF"
+                                                                                
+      $+:.                                                                      
+     I$Z8:                                                                      
+    .O$ZOZ+.                                                                    
+     .OOZOOI                
+      .$$$8?.        THIS SYSTEM WELCOMES YOU
+        .III+.                                                                  
+         .??7+.                                                     .+$.        
+          7?II+=             HAVE A FUCKING DAY                   .?=?II+.      
+          .II77I~~~.                                           .+?=+?IIII.      
+          .??+====~~~~.                                     .====???IIII?.      
+           +=========~~~,                                 ====+???????I?.       
+           +===++++++=+~~~~..                        ..~====+??????????,        
+           =++++??????++++~~~~~~.               ..:~~~====??????II?I??=.        
+           .++++???I?I?????+?+=~~~~~~~~~~~~~~~~~~=~===???????IIIII????.         
+            ~+++?????II?I?????????+=========~===+?????????IIIIIIII??+.          
+             :+++++??IIIIII???????????????????????????????IIIII????~.           
+              ,=+++++??IIII?IIII??????????????????I??IIIIIII?I?I??~.            
+                ~+++++???IIIIIIIIIIII?II??????I????I?IIIII?II???=.              
+                  :=++++????I?IIIIIIIIIIIIIIII?III?I??II?I???+=.                
+                    .~+??????II?IIIIIIIIIIIIIIIIIII??I?IIII+..                  
+                       .,+?IIIIIIIIIIIIIIIIIIIIIIIIIIIII?....                   
+               ...........,:~7IIIIIIIIIIIIIIIIIIIII7?~~:,,,...........          
+              ........,,,:~=+?I7$ZOO88888888OOOZ$7I?++=~::,,,,......            
+                  ...........,,,,,,,,,,,,,,,,,,,,...............                
+                               ..... ..... ...                                  
+
+EOF
+
+}
+
+get_banana
