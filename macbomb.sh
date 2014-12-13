558324fe4082e809f8704440d8c055dce353298d
proto <alessandro@protodigital.net>
2014-12-13 12:34:09 +0100

    add other crap

---
+++ macbomb.sh
@@ -0,0 +1,35 @@
+get_macbomb() { 
+cat <<"EOF"
+                                       .              .     
+                                      .O,           .ck     
+                                        .,        ,:x.      
+                               .:       .o.     ::l.        
+                               .;  c:     'o    ;.          
+                                   .. .x' .'                
+                      lOOOOOOOO.       .         .O' :k  kl 
+                    ;O.        :OOd     ,OOOOd              
+                  :N;              dkkkk,         .         
+                  cM;                  '.   ..   .k;'       
+             ';;;;xMd;;;;'           ,:o'   :c     .o:;     
+             KMMMMMMMMMMMK         ;;:'     ,;       '::,   
+             KMMMMMMMMMMMK         ''       ''         ''   
+         ;kkkWMMMMMMMMMMMWkkk.              lo              
+     .000NMMMMMMMMMMMMMMMMMMMX0K0.           .              
+     'MMMMMMMMMMMMMMMMMMMMMMMMMMM'                          
+    KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK                         
+  .,NMMMM0xXMMMMMMMMMMMMMMMMMMMMMMX,.                       
+  KMMMMMMkcKMMMMMMMMMMMMMMMMMMMMMMMMO                       
+  KMMMMx;kMMMMMMMMMMMMMMMMMMMMMMMMMMO                       
+  KMMMMc lMMMMMMMMMMMMMMMMMMMMMMMMMMO                       
+  KMMMMc lMMMMMMMMMMMMMMMMMMMMMMMMMMO                       
+  KMMMMNXXXWMMMMMMMMMMMMMMMMMMMMMMMMO                       
+  d0WMMMMo.OMMMMMMMMMMMMMMMMMMMMMMW0o                       
+    XMMMMMMMWkkWMMMMMMMMMMMMMMMMMMK                         
+    cxMMMMMMN:cWMMMMMMMMMMMMMMMMMxc                         
+     .MMMMMMMMMMMMMMMMMMMMMMMMMMM.                          
+      ,,,xMMMMMMMMMMMMMMMMMMMo,,,                           
+          ...XMMMMMMMMMMMX...                               
+             .............                                  
+EOF
+}
+get_macbomb
---
+++ sadmac.sh
@@ -0,0 +1,33 @@
+get_sadmac() { 
+cat <<"EOF"
+           .lolloollllllllllllllllllllllllllllo,            
+         .c:                                   .d'.         
+        ,k     .''''''''''''''''''''''''''''     ck         
+        ,k   .c,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;,   ck         
+        ,O   'K'                            dx   ck         
+        ,O   'K'                            dx   ck         
+        ,O   'K'    ,l.'o.        c:.;l     dx   ck         
+        ,k   'K'    .,;;,.        ',:,,     dx   ck         
+        ,k   '0'    .'  ,         .. .'     dd   cx         
+        ,k   '0'         'l.  .l.           ox   cx         
+        ,k   'K'           :dd'             ox   cx         
+        ,k   '0'         .'''''''.          ox   ck         
+        ,k   '0'        :,,,,,,,,';::.      ox   ck         
+        ,k   '0'        .         ...;l     dx   cx         
+        ,k   .k'                     ..     ll   ck         
+        ,k     'llllllllllllllllllllllllllll     ck         
+        ,k                                       ck         
+        ,k                                       ck         
+        ,k                                       ck         
+        ,k    ..                  cooooooooo     ck         
+        ,k   .cc;                                ck         
+        ,k                                       ck         
+        ,k                                       cx         
+          ldlllllllllllllllllllllllllllllllllllox'          
+          xo                                   .0.          
+          xo                                   .K.          
+          xkccccccccccccccccccccccccccccccccccco0.          
+          .''''''''''''''''''''''''''''''''''''''           
+EOF
+}
+get_sadmac
